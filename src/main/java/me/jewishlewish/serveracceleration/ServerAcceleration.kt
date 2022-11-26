1ae148bc90f38d640b7888526cd95241afb89aa1
JewishLewish <lanbrokhman@gmail.com>
2022-11-25 20:17:24 -0800

    Added Config.yml

--- src/main/java/me/jewishlewish/serveracceleration/ServerAcceleration.kt
+++ src/main/java/me/jewishlewish/serveracceleration/ServerAcceleration.kt
@@ -1,10 +1,11 @@
 package me.jewishlewish.serveracceleration
 
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import kotlin.math.log
 
 class ServerAcceleration : JavaPlugin() {
     override fun onEnable() {
-        // Plugin startup logic
         logger.info("\n" +
                 "░██████╗██╗░░░██╗███╗░░██╗██╗░░██╗\n" +
                 "██╔════╝╚██╗░██╔╝████╗░██║╚██╗██╔╝\n" +
@@ -12,6 +13,14 @@ class ServerAcceleration : JavaPlugin() {
                 "░╚═══██╗░░╚██╔╝░░██║╚████║░██╔██╗░\n" +
                 "██████╔╝░░░██║░░░██║░╚███║██╔╝╚██╗\n" +
                 "╚═════╝░░░░╚═╝░░░╚═╝░░╚══╝╚═╝░░╚═╝")
+
+
+        if(File("spigot.yml").exists()){
+            logger.info("spigot.yml does exist.")
+        } else {
+            logger.info("spigot.yml does not exist.")
+        }
+
     }
 
     override fun onDisable() {
