62b630298258ddbdf410f2d2da914a0b3712e55f
JewishLewish <lanbrokhman@gmail.com>
2022-11-25 22:58:37 -0800

    Re-design of code for more efficient output.

--- src/main/java/me/jewishlewish/serveracceleration/fileoptimisation.java
+++ src/main/java/me/jewishlewish/serveracceleration/fileoptimisation.java
@@ -9,28 +9,27 @@ import java.io.PrintWriter;
 public class fileoptimisation {
     static void spigotEdit(String ac) {
         if (ac.equals("fast")) {
-
-            PrintWriter prw= null;
-            try {
-                prw = new PrintWriter("spigot.yml");
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
+            if (new File("spigot.yml").exists()) {
+                PrintWriter prw= null;
+                try {
+                    prw = new PrintWriter("spigot.yml");
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                prw.println(new returndata().files("spigot"));
+                prw.close();
             }
-            prw.println(new returndata().files("spigot"));
-            prw.close();
         }
 
     }
     public void configread(ServerAcceleration plugin) {
-        FileConfiguration config = plugin.getConfig();
+
         String ac = plugin.getConfig().getString("acceleration");
         Bukkit.getLogger().info("Acceleration speed is: " + ac);
 
         if (!"none".equals(ac)) {
             if (plugin.getConfig().getString("spigotedit").equals("true")) {
-                if (new File("spigot.yml").exists()) {
-                    spigotEdit(ac);
-        }
+                spigotEdit(ac);
             }
         }
     }
