97d09e066ea24954688a0b3f775870f14b642b18
JewishLewish <lanbrokhman@gmail.com>
2022-11-25 19:40:20 -0800

    Main Code

---
+++ src/main/java/me/jewishlewish/serveracceleration/ServerAcceleration.java
@@ -0,0 +1,19 @@
+package me.jewishlewish.serveracceleration;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerAcceleration extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        getLogger().info("Hello World");
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
