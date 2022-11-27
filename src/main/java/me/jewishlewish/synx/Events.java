7c423044d7946621c11da8a2c0f47dbae344dd42
JewishLewish <lanbrokhman@gmail.com>
2022-11-27 13:11:48 -0800

    Adjusted Snowman's AI system and block existance of snow.

--- src/main/java/me/jewishlewish/synx/Events.java
+++ src/main/java/me/jewishlewish/synx/Events.java
@@ -1,10 +1,12 @@
 package me.jewishlewish.synx;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Snowman;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.EntityBlockFormEvent;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class Events implements Listener {
@@ -12,7 +14,14 @@ public class Events implements Listener {
     public void SnowNerf(CreatureSpawnEvent event) {
         Entity entity = event.getEntity();
         if (entity instanceof Snowman) {
-            Bukkit.getLogger().info("The creature is a " + entity);
+            ((Snowman) entity).setAI(false);
         }
+    }
+    @EventHandler
+    public void snow(EntityBlockFormEvent e)
+    {
+        if(e.getEntity() instanceof Snowman){}
+            if(e.getNewState().getType() == Material.SNOW)
+                e.setCancelled(true);
     }
 }
