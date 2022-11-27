82d6a1fdb8afbb690e3086356005133d92218320
JewishLewish <lanbrokhman@gmail.com>
2022-11-27 14:23:14 -0800

    removed Imports not used.

--- src/main/java/me/jewishlewish/synx/Events.java
+++ src/main/java/me/jewishlewish/synx/Events.java
@@ -1,13 +1,10 @@
 package me.jewishlewish.synx;
 
-
-
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Snowman;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
-import org.bukkit.plugin.Plugin;
 
 public class Events implements Listener {
 
@@ -18,13 +15,14 @@ public class Events implements Listener {
 
     @EventHandler
     public void SnowNerf(CreatureSpawnEvent event) {
-        if (plugin.getConfig().getString("nerfsnowmans").equals("true")) {
-            CreatureSpawnEvent.getHandlerList().unregister(this);
-        }
+        if (plugin.getConfig().getString("nerfsnowman").equals("true")) {
+            Entity entity = event.getEntity();
+            if (entity instanceof Snowman) {
+                ((Snowman) entity).setAI(false);
+            }
 
-        Entity entity = event.getEntity();
-        if (entity instanceof Snowman) {
-            ((Snowman) entity).setAI(false);
+        } else {
+            CreatureSpawnEvent.getHandlerList().unregister(this);
         }
     }
 }
