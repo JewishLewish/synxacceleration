0ec0ff825b2b049f381e7d42e1c23fee06f165d2
JewishLewish <lanbrokhman@gmail.com>
2022-11-27 20:57:13 -0800

    Bstats last edit.

--- src/main/java/me/jewishlewish/synx/Synx.java
+++ src/main/java/me/jewishlewish/synx/Synx.java
@@ -3,6 +3,8 @@ package me.jewishlewish.synx;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
 public final class Synx extends JavaPlugin {
@@ -10,11 +12,13 @@ public final class Synx extends JavaPlugin {
     @Override
     public void onEnable() {
         Metrics metrics = new Metrics(this,16961);
-        metrics.addCustomChart(new Metrics.SingleLineChart("players", new Callable<Integer>() {
+        metrics.addCustomChart(new Metrics.MultiLineChart("players_and_servers", new Callable<Map<String, Integer>>() {
             @Override
-            public Integer call() throws Exception {
-                // (This is useless as there is already a player chart by default.)
-                return Bukkit.getOnlinePlayers().size();
+            public Map<String, Integer> call() throws Exception {
+                Map<String, Integer> valueMap = new HashMap<>();
+                valueMap.put("servers", 1);
+                valueMap.put("players", Bukkit.getOnlinePlayers().size());
+                return valueMap;
             }
         }));
 
